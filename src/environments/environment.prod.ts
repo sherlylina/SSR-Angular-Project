a3adb0a6289214f0ee16ce8d7eebf25681eaecad
Sherly Anjelina <sherlynjelina@gmail.com>
2022-12-18 01:07:40 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://msa.id/webcore/public',
-  CAPTCHA_SITEKEY: '6LeKG0EaAAAAADJCMLtreWXS8KupmDIn8rANY_rU'
+  API_URL: '',
+  CAPTCHA_SITEKEY: ''
 };
